ae6ab3e755c0a0045a4e2f7bacfce0ad2981f43e
Parsa <parsarahimnia@gmail.com>
2025-05-08 10:14:01 -0400

    initial commit

---
+++ src/main/java/com/example/McgillNotifier.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class McgillNotifier {
+    //hello world
+    public static void main(String[] args) {
+        System.out.println("Hello, McGill Notifier!");
+    }
+}
