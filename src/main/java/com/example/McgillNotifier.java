33ccc2f7c72ce6954063d01b3f7c363ebfe3f0ae
Parsa <parsarahimnia@gmail.com>
2025-05-08 10:24:03 -0400

    this is a setup phase

--- src/main/java/com/example/McgillNotifier.java
+++ src/main/java/com/example/McgillNotifier.java
@@ -1,8 +1,5 @@
 package com.example;
 
-public class McgillNotifier {
+public class McGillNotifier {
     //hello world
-    public static void main(String[] args) {
-        System.out.println("Hello, McGill Notifier!");
-    }
 }
