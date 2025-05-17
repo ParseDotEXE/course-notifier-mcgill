616e16e900626886773b2b54e3cb5a0674e05f5c
Parsa <parsarahimnia@gmail.com>
2025-05-17 15:12:32 -0400

    userrequest class implemented with necessary fields

--- src/main/java/com/vsbnotifier/model/UserRequest.java
+++ src/main/java/com/vsbnotifier/model/UserRequest.java
@@ -1,5 +1,51 @@
 package com.vsbnotifier.model;
 
 public class UserRequest {
+    //declare all fields
+    private String courseCode;
+    private String term;
+    private String phoneNumber;
+    private boolean notified = false;
 
+    //constructor
+    public UserRequest(String courseCode, String term, String phoneNumber) {
+        this.courseCode = courseCode;
+        this.term = term;
+        this.phoneNumber = phoneNumber;
+    }
+    //getters and setters
+    public String getCourseCode() {
+        return courseCode;
+    }
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+    public String getTerm() {
+        return term;
+    }
+    public void setTerm(String term) {
+        this.term = term;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public boolean isNotified() {
+        return notified;
+    }
+    public void setNotified(boolean notified) {
+        this.notified = notified;
+    }
+    //to have hman readable output
+    @Override
+    public String toString(){
+        return "UserRequest{" +
+                "courseCode='" + courseCode + '\'' +
+                ", term='" + term + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", notified=" + notified +
+                '}';
+    }
 }
