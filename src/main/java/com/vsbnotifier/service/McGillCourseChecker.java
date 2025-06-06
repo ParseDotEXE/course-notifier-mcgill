9793c6748e77f6dfe589816c5d88ca003cf7305c
Parsa <parsarahimnia@gmail.com>
2025-06-06 17:34:08 -0400

    I found out the pattern in the vsb system. Each course gets its own request with va_X_0 where X = course index and Batch request (refresh): All courses combined into one request with multiple va_X_0 parameters. I will only be selecting 1 course for notifications at a time so I only care about va_0_0 for now

--- src/main/java/com/vsbnotifier/service/McGillCourseChecker.java
+++ src/main/java/com/vsbnotifier/service/McGillCourseChecker.java
@@ -1,31 +1,44 @@
 package com.vsbnotifier.service;
 
 //add necessary imports
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.net.CookieManager;
-import java.net.CookiePolicy;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.io.IOException;
-import java.io.StringReader;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Element;
-import org.xml.sax.InputSource;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.vsbnotifier.model.CourseInfo;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import com.vsbnotifier.model.CourseInfo; //import the CourseInfo model
 import com.vsbnotifier.model.SectionInfo; //import the SectionInfo model
 
+import java.time.Duration;
+
 public class McGillCourseChecker {
-    private static final String API_URL = "https://vsb.mcgill.ca/api/class-data";
-    
+    ChromeOptions options = new ChromeOptions(); //to set up the ChromeDriver options
+    public CourseInfo checkCouseAvailability(String term, String courseCode) throws Exception {
+        //set up the ChromeDriver options
+        WebDriver driver = new ChromeDriver(options);
+        CourseInfo courseInfo = new CourseInfo(); //create a new CourseInfo object
+        driver.get("https://vsb.mcgill.ca/criteria.jsp");
+        //TODO: selenium logic to navigate page, get course infomation
+        
+        //click on the correct term
+        driver.findElement(By.linkText(term)).click();
+        //click on the box to select the course
+        driver.findElement(By.id("code_number")).sendKeys(courseCode); //click and enter the course code
+        driver.findElement(By.id("code_number")).sendKeys(Keys.ENTER); //click enter
+        //create a new waitdriver object
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3)); //wait for 3 seconds
+        
+        return courseInfo;
+    }
 }
